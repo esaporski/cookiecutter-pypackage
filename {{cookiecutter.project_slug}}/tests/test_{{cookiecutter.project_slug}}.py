8eb16344536df833c7e54e86be0ce69fe29445ce
Eduardo B. Saporski <esaporski@protonmail.com>
2023-05-04 15:31:40 -0300

    feat: migrate to ruff from flake8/isort

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -19,9 +19,10 @@ from {{ cookiecutter.project_slug }} import cli
 {%- if cookiecutter.use_pytest == 'y' %}
 
 
-@pytest.fixture
+@pytest.fixture()
 def response():
-    """Sample pytest fixture.
+    """
+    Sample pytest fixture.
 
     See more at: http://doc.pytest.org/en/latest/fixture.html
     """
@@ -29,14 +30,14 @@ def response():
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
+def test_content(response) -> None:
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 
 
-def test_command_line_interface():
+def test_command_line_interface() -> None:
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
@@ -52,24 +53,24 @@ def test_command_line_interface():
 class Test{{ cookiecutter.project_slug|title }}(unittest.TestCase):
     """Tests for `{{ cookiecutter.project_slug }}` package."""
 
-    def setUp(self):
+    def setUp(self) -> None:
         """Set up test fixtures, if any."""
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
+    def test_000_something(self) -> None:
         """Test something."""
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 
-    def test_command_line_interface(self):
+    def test_command_line_interface(self) -> None:
         """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
-        assert '{{ cookiecutter.project_slug }}.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
+        assert "{{ cookiecutter.project_slug }}.cli.main" in result.output
+        help_result = runner.invoke(cli.main, ["--help"])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert "--help  Show this message and exit." in help_result.output
 {%- endif %}
 {%- endif %}
